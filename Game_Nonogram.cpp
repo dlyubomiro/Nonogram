eb92a780e92bda7a453b6a3602aaf2c7253f65f7
Desislava Lyubomirova <desita03@abv.bg>
2023-01-17 22:20:17 +0200

    Added header comment

--- Game_Nonogram.cpp
+++ Game_Nonogram.cpp
@@ -1,6 +1,16 @@
-// Nonogram_Final.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/**
+*
+* Solution to course project # 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Desislava Lyubomirova
+* @idnumber 7MI0600144 @compiler VC
+*
+* Main source file
+*
+*/
 
 #include <iostream>
 #include <fstream>>
@@ -655,14 +665,3 @@ int main()
 		cout << "Mistakes: " << mistakes << endl;
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
